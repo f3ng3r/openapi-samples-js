d53b49d4483de64932f0072491fd400c077b68b0
Frederik Fenger <frederikfenger@gmail.com>
2020-03-24 12:47:05 +0100

    Update demo.js

--- instruments/instrument-retrieval/demo.js
+++ instruments/instrument-retrieval/demo.js
@@ -1,38 +1,32 @@
 /*jslint this: true, browser: true, for: true, long: true */
-/*global window console accountKey run processError processNetworkError */
+/*global window console accountKey run processError */
 
-var requestCount = 0;
-var requestQueue = [];
-var timer;
-var instrumentIds = [];
+let requestCount = 0;
+let timer;
+const requestQueue = [];
+const instrumentIds = [];
 
-function processDetailResponse(assetType, responseJson) {
-    var i;
-    for (i = 0; i < responseJson.Data.length; i += 1) {
-        instrumentIds.push(responseJson.Data[i].Uic);
-    }
+function updateOutput() {
     document.getElementById("idInstruments").value = instrumentIds.join(",");
     console.log("Found in total " + instrumentIds.length + " instruments..");
-    console.log("Added " + responseJson.OptionSpace.length + " instruments type " + assetType + ", total " + instrumentIds.length + " instruments..");
+}
+
+function processDetailResponse(assetType, responseJson) {
+    const uics = responseJson.Data.map(i => i.Uic)
+    instrumentIds.push(...uics)
+    updateOutput()
 }
 
 function processContractOptionSpace(assetType, responseJson) {
-    var i;
-    var j;
-    for (i = 0; i < responseJson.OptionSpace.length; i += 1) {
-        for (j = 0; j < responseJson.OptionSpace[i].SpecificOptions.length; j += 1) {
-            instrumentIds.push(responseJson.OptionSpace[i].SpecificOptions[j].Uic);
-        }
-    }
-    document.getElementById("idInstruments").value = instrumentIds.join(",");
-    console.log("Added " + responseJson.OptionSpace.length + " instruments type " + assetType + ", total " + instrumentIds.length + " instruments..");
+    const uics = responseJson.OptionSpace.map(os => os.SpecificOptions.map(so => so.Uic))
+    instrumentIds.push(...uics)
+    updateOutput()
 }
 
 function processSearchResponse(assetType, responseJson) {
-    var i;
-    var baseUrl = "https://gateway.saxobank.com/sim/openapi/ref/v1/instruments/details?AccountKey=" + encodeURIComponent(accountKey) + "&$top=1000&AssetTypes=" + assetType + "&Uics=";
-    var url = "";
-    var separator = encodeURIComponent(",");
+    const baseUrl = "https://gateway.saxobank.com/sim/openapi/ref/v1/instruments/details?AccountKey=" + encodeURIComponent(accountKey) + "&$top=1000&AssetTypes=" + assetType + "&Uics=";
+    let url = "";
+    const separator = encodeURIComponent(",");
 
     function addToQueue() {
         requestQueue.push({
@@ -44,26 +38,24 @@ function processSearchResponse(assetType, responseJson) {
 
     console.log("Found " + responseJson.Data.length + " instruments on this exchange");
     // We have the Uic - collect the details
-    for (i = 0; i < responseJson.Data.length; i += 1) {
+
+    responseJson.Data.forEach(result => {
         if (assetType === "StockOption" || assetType === "StockIndexOption") {
             // We found an OptionRoot - this must be converted to Uic
             requestQueue.push({
                 "assetType": assetType,
-                "url": "https://gateway.saxobank.com/sim/openapi/ref/v1/instruments/contractoptionspaces/" + responseJson.Data[i].Identifier + "?OptionSpaceSegment=AllDates",
+                "url": "https://gateway.saxobank.com/sim/openapi/ref/v1/instruments/contractoptionspaces/" + result.Identifier + "?OptionSpaceSegment=AllDates",
                 "callback": processContractOptionSpace
             });
         } else {
-            url += (
-                url === ""
-                ? baseUrl
-                : separator
-            ) + responseJson.Data[i].Identifier;
+            url += (url === "" ? baseUrl : separator) + result.Identifier;
             if (url.length > 2000) {
                 addToQueue();
                 url = "";
             }
         }
-    }
+    })
+    
     if (url !== "") {
         addToQueue();
     }
@@ -79,15 +71,19 @@ function processSearchResponse(assetType, responseJson) {
 }
 
 function processExchangesResponse(assetType, responseJson) {
-    var i;
     console.log("Found " + responseJson.Data.length + " exchanges, starting to collect instrument ids");
     for (i = 0; i < responseJson.Data.length; i += 1) {
+        
+    }
+
+    responseJson.Data.forEach(exchange => {
         requestQueue.push({
             "assetType": assetType,
-            "url": "https://gateway.saxobank.com/sim/openapi/ref/v1/instruments?ExchangeId=" + encodeURIComponent(responseJson.Data[i].ExchangeId) + "&AssetTypes=" + assetType + "&IncludeNonTradable=false&$top=1000&AccountKey=" + encodeURIComponent(accountKey),
+            "url": "https://gateway.saxobank.com/sim/openapi/ref/v1/instruments?ExchangeId=" + encodeURIComponent(exchange.ExchangeId) + "&AssetTypes=" + assetType + "&IncludeNonTradable=false&$top=1000&AccountKey=" + encodeURIComponent(accountKey),
             "callback": processSearchResponse
         });
-    }
+    })
+
 }
 
 /**
@@ -99,18 +95,15 @@ function start() {
         "assetType": "ContractFutures",
         "url": "https://gateway.saxobank.com/sim/openapi/ref/v1/exchanges?$top=1000",
         "callback": processExchangesResponse
-    });
-    requestQueue.push({
+    },{
         "assetType": "Stock",
         "url": "https://gateway.saxobank.com/sim/openapi/ref/v1/exchanges?$top=1000",
         "callback": processExchangesResponse
-    });
-    requestQueue.push({
+    },{
         "assetType": "StockOption",
         "url": "https://gateway.saxobank.com/sim/openapi/ref/v1/exchanges?$top=1000",
         "callback": processExchangesResponse
-    });
-    requestQueue.push({
+    },{
         "assetType": "StockIndexOption",
         "url": "https://gateway.saxobank.com/sim/openapi/ref/v1/exchanges?$top=1000",
         "callback": processExchangesResponse
@@ -118,7 +111,7 @@ function start() {
 }
 
 function runJobFromQueue() {
-    var job;
+    let job;
     if (requestQueue.length > 0) {
         job = requestQueue.shift();
         document.getElementById("idResponse").innerText = "Processing job for AssetType " + job.assetType + ":\r\n" + job.url + "\r\nRequests: " + requestCount + "\r\nJobs in queue: " + requestQueue.length;
@@ -147,7 +140,7 @@ function runJobFromQueue() {
 }
 
 (function () {
-    var refLimitPerMinute = 60;
+    const refLimitPerMinute = 60;
     timer = setInterval(runJobFromQueue, (refLimitPerMinute / 60 * 1000) + 25);  // A little more, to prevent risk of 429 TooManyRequests
 
     document.getElementById("idBtnStart").addEventListener("click", function () {
